13526a1aedca9e49613f38114209c7f0c114e25d
Bruce Leong <brucewleong@gmail.com>
2018-08-08 17:39:54 -0400

    setup

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
-const os = require('os');
+const volleyball = require('volleyball');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const fs = require('fs')
+const Papa = require('papaparse')
 
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/api/stuff', (req, res, next) => {
+  console.log('hitting the proper route')
+  res.send({data: 'hellloooo'});
+})
+
 app.listen(8080, () => console.log('Listening on port 8080!'));
